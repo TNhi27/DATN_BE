5fb5a56bd78abf843e09396876a52a425d42eff0
TNhi27 <truongta.270201@gmail.com>
2021-10-08 22:07:53 +0700

    init

---
+++ src/main/java/com/okteam/DatnBeApplication.java
@@ -0,0 +1,13 @@
+package com.okteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatnBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatnBeApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/okteam/restcontroller/demo.java
@@ -0,0 +1,12 @@
+package com.okteam.restcontroller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class demo {
+	@GetMapping("/get")
+	public String name() {
+		return "hello";
+	}
+}
