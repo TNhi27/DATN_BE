2380c39e503fabe46e2c46ec586bd071b8bf4248
Trần Phước Định <dinhtppc00576@fpt.edu.vn>
2021-10-30 14:44:43 +0700

    Tạo api thêm loại

--- src/main/java/com/okteam/restcontroller/CategoryRestController.java
+++ src/main/java/com/okteam/restcontroller/CategoryRestController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +24,12 @@ public class CategoryRestController {
 	@GetMapping("/category/list")
 	public List<Category> getCategories(){
 		return categoryRepo.findAll();
+	}
+	
+	@PostMapping("/category/add")
+	public String addCategory(@RequestBody Category category) {
+		categoryRepo.save(category);
+		return "Thêm loại thành công!";
 	}
 	
 }
