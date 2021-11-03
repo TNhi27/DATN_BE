a2d13ed2b669f51c37487a092c1c0fe19cc58b3b
Trần Phước Định <dinhtppc00576@fpt.edu.vn>
2021-11-03 23:00:46 +0700

    Viết api thêm nhãn hàng

--- src/main/java/com/okteam/restcontroller/BrandController.java
+++ src/main/java/com/okteam/restcontroller/BrandController.java
@@ -6,27 +6,34 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.okteam.dao.BrandRepository;
+import com.okteam.dao.CategoryRepository;
 import com.okteam.entity.Brand;
+import com.okteam.entity.Category;
 import com.okteam.entity.Response;
 
 @CrossOrigin("*")
 @RequestMapping("/api/v1")
 @RestController
-public class BrandRestController {
+public class BrandController {
 	
 	@Autowired
 	BrandRepository brandRepo;
+	@Autowired
+	CategoryRepository categoryRepo;
 	
 	@GetMapping("/brand/list")
 	public Response<Brand> getBrands(@RequestParam(value="idcate", required = false) String idcate){
 		String message = "Không lấy được dữ liệu";
 		List<Brand>list = new ArrayList<>();
-		if(idcate == null || idcate == "0") {
+		if(idcate == null || idcate.equals("0") ) {
 			list = brandRepo.findAll();
 			message = "OK";
 		} else {
@@ -34,5 +41,14 @@ public class BrandRestController {
 			message = "OK";
 		}
 		return new Response<Brand>(list, message);
+	}
+	
+	@PostMapping("/brand/addTo/{idcate}")
+	public Response<Brand> addBrand(@PathVariable("idcate") String idcate, @RequestBody Brand brand){
+		String message = "OK";
+		Category c = categoryRepo.findById(idcate).get();
+		brand.setBr_category(c);
+		brandRepo.save(brand);
+		return new Response<Brand>(brandRepo.findAll(), message);
 	}
 }
