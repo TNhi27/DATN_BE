334dc1f27ba26a239a3b119fcd2dbabec8113e5f
Lưu Trường Tá <76809671+TNhi27@users.noreply.github.com>
2021-11-03 14:52:47 +0700

    Update CommentRepository.java

--- src/main/java/com/okteam/dao/CommentRepository.java
+++ src/main/java/com/okteam/dao/CommentRepository.java
@@ -1,13 +1,12 @@
 package com.okteam.dao;
 
-<<<<<<< HEAD
+
 import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-=======
 
->>>>>>> 6c1e618fb3e2acbc76f3c8273d9de874ed1079b0
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
