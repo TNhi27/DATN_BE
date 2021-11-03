a483324f1373d45729a292c4c98c29f7a1a24dc2
Lưu Trường Tá <76809671+TNhi27@users.noreply.github.com>
2021-11-03 14:52:08 +0700

    Update CategoryRepository.java

--- src/main/java/com/okteam/dao/CategoryRepository.java
+++ src/main/java/com/okteam/dao/CategoryRepository.java
@@ -7,10 +7,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, String> {
-<<<<<<< HEAD
 
-	
-=======
 	List<Category> findByParent(String parent);
->>>>>>> 6c1e618fb3e2acbc76f3c8273d9de874ed1079b0
+
 }
