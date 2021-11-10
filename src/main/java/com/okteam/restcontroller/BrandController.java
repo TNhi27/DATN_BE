6db986516a6f50163729c67a988cc9d37dc9ca2c
Trần Phước Định <dinhtppc00576@fpt.edu.vn>
2021-11-10 22:01:20 +0700

    Viết api sửa và xóa nhãn hàng

--- src/main/java/com/okteam/restcontroller/BrandController.java
+++ src/main/java/com/okteam/restcontroller/BrandController.java
@@ -5,9 +5,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,17 +25,17 @@ import com.okteam.entity.Response;
 @RequestMapping("/api/v1")
 @RestController
 public class BrandController {
-	
+
 	@Autowired
 	BrandRepository brandRepo;
 	@Autowired
 	CategoryRepository categoryRepo;
-	
+
 	@GetMapping("/brand/list")
-	public Response<Brand> getBrands(@RequestParam(value="idcate", required = false) String idcate){
+	public Response<Brand> getBrands(@RequestParam(value = "idcate", required = false) String idcate) {
 		String message = "Không lấy được dữ liệu";
-		List<Brand>list = new ArrayList<>();
-		if(idcate == null || idcate.equals("0") ) {
+		List<Brand> list = new ArrayList<>();
+		if (idcate == null || idcate.equals("0")) {
 			list = brandRepo.findAll();
 			message = "OK";
 		} else {
@@ -42,13 +44,54 @@ public class BrandController {
 		}
 		return new Response<Brand>(list, message);
 	}
-	
+
 	@PostMapping("/brand/addTo/{idcate}")
-	public Response<Brand> addBrand(@PathVariable("idcate") String idcate, @RequestBody Brand brand){
+	public Response<Brand> addBrand(@PathVariable("idcate") String idcate, @RequestBody Brand brand) {
 		String message = "OK";
+		boolean check = brandRepo.findByIdcate(idcate).stream()
+				.allMatch(br -> !brand.getName().equalsIgnoreCase(br.getName()));
+		if (!check) {
+			message = "Tên nhãn hàng đã tồn tại trong loại hàng này!";
+			return new Response<Brand>(brandRepo.findByIdcate(idcate), message);
+		}
 		Category c = categoryRepo.findById(idcate).get();
 		brand.setBr_category(c);
 		brandRepo.save(brand);
-		return new Response<Brand>(brandRepo.findAll(), message);
+		return new Response<Brand>(brandRepo.findByIdcate(idcate), message);
+	}
+
+	@DeleteMapping("/brand/delete/{id}/{idcate}")
+	public Response<Brand> deleteBrand(@PathVariable("id") Integer id, @PathVariable("idcate") String idcate) {
+		String message = "Không tìm thấy nhãn hàng!";
+		if (brandRepo.existsById(id)) {
+			if (brandRepo.findById(id).get().getProducts().size() > 0) {
+				message = "Nhãn hàng đã có sản phẩm!";
+			} else {
+				brandRepo.deleteById(id);
+				message = "OK";
+			}
+		}
+		return new Response<Brand>(brandRepo.findByIdcate(idcate), message);
+	}
+
+	@PutMapping("/brand/update")
+	public Response<Brand> updateBrand(@RequestParam("id") Integer id, @RequestParam("value") String value) {
+		String message = "OK";
+		List<Brand>list = new ArrayList<>();
+		Brand brand = brandRepo.findById(id).get();
+		if (value.isEmpty()) {
+			message = "Không được đễ trống tên";
+		} else {
+			String idcate = brandRepo.findById(id).get().getBr_category().getIdcate();
+			boolean check = brandRepo.findByIdcate(idcate).stream().allMatch(br -> !value.equalsIgnoreCase(br.getName()));
+			if (!check) {
+				message = "Tên nhãn hàng đã tồn tại trong loại hàng này!";
+			} else {
+				brand.setName(value);
+				brandRepo.save(brand);
+			}
+		}
+		list.add(brand);
+		return new Response<Brand>(list, message);
 	}
 }
